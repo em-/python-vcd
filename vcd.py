1804073c9ef223a7a5396ab514e5438663991e20
Emanuele Aina <em@nerd.ocracy.org>
2007-10-11 20:47:43 +0200

    Removed whitespace in excess

--- vcd.py
+++ vcd.py
@@ -17,10 +17,10 @@ identifier = Word(printables)('id')
 definition = Word(alphas)('type') + Word(nums)('size') + \
              identifier + Word(printables)('name')
 
-signal     = Group(s('$var') + definition + s('$end'))('signal')
+signal = Group(s('$var') + definition + s('$end'))('signal')
 
-content      = SkipTo('$end')('content') + s('$end')
-section      = Group(s('$') + Word(alphas)('name') + content)('section')
+content = SkipTo('$end')('content') + s('$end')
+section = Group(s('$') + Word(alphas)('name') + content)('section')
 
 unit      = s('1') + oneOf('s ms ns us ps fs')
 timescale = (s('$timescale') + unit + s('$end'))('timescale')
@@ -35,8 +35,8 @@ time = s('#') + Word(nums)('time')
 std_logic        = oneOf('U X 0 1 Z W L H-')('std_logic')
 std_logic_vector = Word('b', 'UX01ZWLH-')('std_logic_vector')
 
-value  = Group((std_logic | std_logic_vector ) + identifier)('value')
-step   = Group(time + ZeroOrMore(value))('step')
+value = Group((std_logic | std_logic_vector ) + identifier)('value')
+step  = Group(time + ZeroOrMore(value))('step')
 
 headers = signal | timescale | scope | upscope
 changes = enddefinitions + ZeroOrMore(step) + StringEnd()
