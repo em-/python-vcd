c940e4ec9b54612c0be4466f5fa4b1d2793ecd04
Emanuele Aina <em@nerd.ocracy.org>
2007-10-11 17:15:15 +0200

    Give names to all the parsed elements

--- vcd.py
+++ vcd.py
@@ -7,15 +7,17 @@ if len(sys.argv) != 2:
     print "Usage: %s FILE" % sys.argv[0]
     sys.exit(2)
 
-signal_id = Word(printables).setResultsName('signal_id')
-signal_definition = Word(alphas) + Word(nums) + signal_id + Word(printables)
-signal = Group(Suppress('$') + Literal('var') + signal_definition + Suppress('$end'))
-section = Group(Suppress('$') + Word(alphas) + SkipTo('$end') + Suppress('$end')).setResultsName('section')
+word = Word(alphas)
+
+signal_id = Word(printables).setResultsName('identifier')
+signal_definition = word.setResultsName('type') + Word(nums).setResultsName('size') + signal_id + Word(printables).setResultsName('name')
+signal = (Group(Suppress('$') + Literal('var').setResultsName('var') + signal_definition + Suppress('$end'))).setResultsName('signal')
+section = Group(Suppress('$') + word.setResultsName('name') + SkipTo('$end').setResultsName('content') + Suppress('$end')).setResultsName('section')
 
 time = Suppress(Literal('#')) + Word(nums).setResultsName('time')
 
 std_logic = oneOf('U X 0 1 Z W L H-').setResultsName('std_logic')
-std_logic_vector = Group(Literal('b') + Word('UX01ZWLH-')).setResultsName('std_logic_vector')
+std_logic_vector = Group(Literal('b').setResultsName('encoding') + Word('UX01ZWLH-').setResultsName('values')).setResultsName('std_logic_vector')
 value = (Group(std_logic + signal_id) | Group(std_logic_vector + signal_id)).setResultsName('value')
 
 change = Group(time + ZeroOrMore(value)).setResultsName('change')
