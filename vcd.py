876048277c7690d3e6b3afddf100e70c6f223e60
Emanuele Aina <em@nerd.ocracy.org>
2007-10-11 15:17:28 +0200

    Parse the signal definitions

--- vcd.py
+++ vcd.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 
 import sys
-from pyparsing import Word, Group, SkipTo, Suppress, ZeroOrMore, alphas
+from pyparsing import Word, Group, SkipTo, Literal, Suppress, ZeroOrMore, alphas, nums, printables
 
 if len(sys.argv) != 2:
     print "Usage: %s FILE" % sys.argv[0]
     sys.exit(2)
 
+signal_definition = Word(alphas) + Word(nums) + Word(printables) + Word(printables)
+signal = Group(Suppress('$') + Literal('var') + signal_definition + Suppress('$end'))
 section = Group(Suppress('$') + Word(alphas) + SkipTo('$end') + Suppress('$end'))
 
-vcd = ZeroOrMore(section)
+vcd = ZeroOrMore(signal | section)
 
 print vcd.parseFile(sys.argv[1]).asXML()
