d7aeaf685afef57fe6635e690ab1c049480128f0
Emanuele Aina <em@nerd.ocracy.org>
2007-10-11 18:20:52 +0200

    Made s() and g() shortcuts for Suppress() and Group()

--- vcd.py
+++ vcd.py
@@ -10,25 +10,28 @@ if len(sys.argv) != 2:
     print "Usage: %s FILE" % sys.argv[0]
     sys.exit(2)
 
+s = Suppress
+g = Group
+
 type = Word(alphas).setResultsName('type')
 size = Word(nums).setResultsName('size')
 id = Word(printables).setResultsName('id')
 name = Word(printables).setResultsName('name')
 
 signal_definition = type + size + id + name
-signal = (Group(Suppress('$var') + signal_definition + Suppress('$end'))).setResultsName('signal')
+signal = (g(s('$var') + signal_definition + s('$end'))).setResultsName('signal')
 
-content = SkipTo('$end').setResultsName('content') + Suppress('$end')
+content = SkipTo('$end').setResultsName('content') + s('$end')
 section_name = Word(alphas).setResultsName('name')
-section = Group(Suppress('$') + section_name + content).setResultsName('section')
+section = g(s('$') + section_name + content).setResultsName('section')
 
-time = Suppress('#') + Word(nums).setResultsName('time')
+time = s('#') + Word(nums).setResultsName('time')
 
 std_logic = oneOf('U X 0 1 Z W L H-').setResultsName('std_logic')
 std_logic_vector = Word('b', 'UX01ZWLH-').setResultsName('std_logic_vector')
-value = (Group(std_logic + id) | Group(std_logic_vector + id)).setResultsName('value')
+value = (g(std_logic + id) | g(std_logic_vector + id)).setResultsName('value')
 
-change = Group(time + ZeroOrMore(value)).setResultsName('change')
+change = g(time + ZeroOrMore(value)).setResultsName('change')
 
 vcd = (ZeroOrMore(signal | section) + ZeroOrMore(change)).setResultsName('vcd')
 
