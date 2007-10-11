14fccab6cff9c9ddfe086167f497b854074f66a5
Emanuele Aina <em@nerd.ocracy.org>
2007-10-11 18:41:13 +0200

    Aligned and grouped some definitions better

--- vcd.py
+++ vcd.py
@@ -14,22 +14,22 @@ s = Suppress
 
 type = Word(alphas)('type')
 size = Word(nums)('size')
-id = Word(printables)('id')
+id   = Word(printables)('id')
 name = Word(printables)('name')
 
 definition = type + size + id + name
-signal = Group(s('$var') + definition + s('$end'))('signal')
+signal     = Group(s('$var') + definition + s('$end'))('signal')
 
-content = SkipTo('$end')('content') + s('$end')
+content      = SkipTo('$end')('content') + s('$end')
 section_name = Word(alphas)('name')
-section = Group(s('$') + section_name + content)('section')
+section      = Group(s('$') + section_name + content)('section')
 
 time = s('#') + Word(nums)('time')
 
-std_logic = oneOf('U X 0 1 Z W L H-')('std_logic')
+std_logic        = oneOf('U X 0 1 Z W L H-')('std_logic')
 std_logic_vector = Word('b', 'UX01ZWLH-')('std_logic_vector')
-value = Group(std_logic + id) | Group(std_logic_vector + id)('value')
 
+value  = Group(std_logic + id) | Group(std_logic_vector + id)('value')
 change = Group(time + ZeroOrMore(value))('change')
 
 vcd = (ZeroOrMore(signal | section) + ZeroOrMore(change))('vcd')
