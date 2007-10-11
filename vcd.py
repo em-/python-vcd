bb6d5f43cc49f0a723091999b445a5e483368745
Emanuele Aina <em@nerd.ocracy.org>
2007-10-11 16:20:11 +0200

    Add names to the tokens to make the XML output more clear

--- vcd.py
+++ vcd.py
@@ -7,19 +7,19 @@ if len(sys.argv) != 2:
     print "Usage: %s FILE" % sys.argv[0]
     sys.exit(2)
 
-signal_id = Word(printables)
+signal_id = Word(printables).setResultsName('signal_id')
 signal_definition = Word(alphas) + Word(nums) + signal_id + Word(printables)
 signal = Group(Suppress('$') + Literal('var') + signal_definition + Suppress('$end'))
-section = Group(Suppress('$') + Word(alphas) + SkipTo('$end') + Suppress('$end'))
+section = Group(Suppress('$') + Word(alphas) + SkipTo('$end') + Suppress('$end')).setResultsName('section')
 
-time = Suppress(Literal('#')) + Word(nums)
+time = Suppress(Literal('#')) + Word(nums).setResultsName('time')
 
-std_logic = oneOf('U X 0 1 Z W L H-')
-std_logic_vector = Group(Literal('b') + Word('UX01ZWLH-'))
-value = Group(std_logic + signal_id) | Group(std_logic_vector + signal_id)
+std_logic = oneOf('U X 0 1 Z W L H-').setResultsName('std_logic')
+std_logic_vector = Group(Literal('b') + Word('UX01ZWLH-')).setResultsName('std_logic_vector')
+value = (Group(std_logic + signal_id) | Group(std_logic_vector + signal_id)).setResultsName('value')
 
-change = Group(time + ZeroOrMore(value))
+change = Group(time + ZeroOrMore(value)).setResultsName('change')
 
-vcd = ZeroOrMore(signal | section) + ZeroOrMore(change)
+vcd = (ZeroOrMore(signal | section) + ZeroOrMore(change)).setResultsName('vcd')
 
 print vcd.parseFile(sys.argv[1]).asXML()
