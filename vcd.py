2656804cf51d1bb07e5b7843e726b81aa2cbfd12
Emanuele Aina <em@nerd.ocracy.org>
2007-10-11 18:33:40 +0200

    Deleted the g() shortcut as it was no longer needed

--- vcd.py
+++ vcd.py
@@ -11,7 +11,6 @@ if len(sys.argv) != 2:
     sys.exit(2)
 
 s = Suppress
-g = Group
 
 type = Word(alphas)('type')
 size = Word(nums)('size')
@@ -19,19 +18,19 @@ id = Word(printables)('id')
 name = Word(printables)('name')
 
 definition = type + size + id + name
-signal = g(s('$var') + definition + s('$end'))('signal')
+signal = Group(s('$var') + definition + s('$end'))('signal')
 
 content = SkipTo('$end')('content') + s('$end')
 section_name = Word(alphas)('name')
-section = g(s('$') + section_name + content)('section')
+section = Group(s('$') + section_name + content)('section')
 
 time = s('#') + Word(nums)('time')
 
 std_logic = oneOf('U X 0 1 Z W L H-')('std_logic')
 std_logic_vector = Word('b', 'UX01ZWLH-')('std_logic_vector')
-value = g(std_logic + id) | g(std_logic_vector + id)('value')
+value = Group(std_logic + id) | Group(std_logic_vector + id)('value')
 
-change = g(time + ZeroOrMore(value))('change')
+change = Group(time + ZeroOrMore(value))('change')
 
 vcd = (ZeroOrMore(signal | section) + ZeroOrMore(change))('vcd')
 
