9b1622adbb17369b6246758b1e2e37283b43bba8
Emanuele Aina <em@nerd.ocracy.org>
2007-10-12 13:51:47 +0200

    Added a Signal object

--- vcd/vcd.py
+++ vcd/vcd.py
@@ -2,11 +2,20 @@
 
 import grammar
 
+class Signal(object):
+    def __init__(self, var_type, size, reference):
+        self.type = var_type
+        self.size = size
+        self.reference = reference
+
+    def __str__(self):
+        return "Signal(%s, %s, %s)" % (self.type, self.size, self.reference)
+
 class Vcd(object):
     def __init__(self, filename):
         parse_tree = grammar.vcd.parseFile(filename)
 
-        self.signals = []
+        self.signals = {}
         for i in parse_tree:
             if hasattr(i, 'getName') and i.getName() == 'signal':
-                self.signals.append(i)
+                self.signals[i[2]] = Signal(i[0], i[1], i[3])
