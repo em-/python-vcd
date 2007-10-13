9068ea45bff37c640ee70f958379c3501823be68
Emanuele Aina <em@nerd.ocracy.org>
2007-10-13 12:48:36 +0200

    Store the original scope in each Signal

--- vcd/vcd.py
+++ vcd/vcd.py
@@ -3,27 +3,37 @@
 import grammar
 
 class Signal(object):
-    def __init__(self, var_type, size, reference):
+    def __init__(self, var_type, size, reference, module):
         self.type = var_type
         self.size = size
         self.reference = reference
+        self.module = module
         self.steps = []
 
     def step(self, time, value):
         self.steps.append((time, value))
 
     def __str__(self):
-        return "Signal(%s, %s, %s)" % (self.type, self.size, self.reference)
+        return "Signal(%s, %s, %s, %s)" % (self.type, self.size, self.reference, self.module)
 
 class Vcd(object):
     def __init__(self, filename):
         parse_tree = grammar.vcd.parseFile(filename)
 
         self.signals = {}
+        scope = []
         for i in parse_tree:
             if not hasattr(i, 'getName'): continue
+            if i.getName() == 'scope':
+                scope.append(i.module)
+            if i.getName() == 'upscope':
+                scope.pop()
             if i.getName() == 'signal':
-                self.signals[i.id] = Signal(i.type, i.size, i.name)
+                if scope:
+                    current_scope = scope[-1]
+                else:
+                    current_scope = None
+                self.signals[i.id] = Signal(i.type, i.size, i.name, current_scope)
 
         for i in parse_tree:
             if hasattr(i, 'getName') and i.getName() == 'step':
